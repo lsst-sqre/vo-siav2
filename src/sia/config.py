a015dd4b5cdef33d37e615a1b0c810fcbd9992ec
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2024-10-08 16:40:48 -0700

    Add sia dir

---
+++ src/sia/config.py
@@ -0,0 +1,33 @@
+"""Configuration definition."""
+
+from __future__ import annotations
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from safir.logging import LogLevel, Profile
+
+__all__ = ["Config", "config"]
+
+
+class Config(BaseSettings):
+    """Configuration for sia."""
+
+    name: str = Field("sia", title="Name of application")
+
+    path_prefix: str = Field("/sia", title="URL prefix for application")
+
+    profile: Profile = Field(
+        Profile.development, title="Application logging profile"
+    )
+
+    log_level: LogLevel = Field(
+        LogLevel.INFO, title="Log level of the application's logger"
+    )
+
+    model_config = SettingsConfigDict(
+        env_prefix="VO_SIAV2_", case_sensitive=False
+    )
+
+
+config = Config()
+"""Configuration for sia."""
